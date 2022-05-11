5bcc8192421737559485f71ccb53d61ec2ee6aa1
prithvi <prith189@gmail.com>
2022-05-11 12:20:02 -0700

    Added download links to script

--- main.py
+++ main.py
@@ -101,12 +101,26 @@ class Prediction:
         self.features = np.load(features_file)
         self.df_idx = np.load(idx_file)
         
+        #Download link for features_file
+        #!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-3gKYoipfdPkeQHnHa0M2vD7QEug5wNS' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-3gKYoipfdPkeQHnHa0M2vD7QEug5wNS" -O all-the-news-embeddings-title.npy && rm -rf /tmp/cookies.txt
+        
+        #Download link for idx_file
+        #!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-5IsScXPtUY5jXVe_83RuQ0uI7eQqDMI' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-5IsScXPtUY5jXVe_83RuQ0uI7eQqDMI" -O all-the-news-embeddings-title-index.npy && rm -rf /tmp/cookies.txt
+        
+        
+        
         print('Loading Dimensionality reduction model')
         #Load the dimensionality reduction model
         dim_red_embeddings_file = os.path.join(BASE_PATH, 'all-the-news-embeddings-title-umap.npy')
         umap_model_file = os.path.join(BASE_PATH, 'umap-model.sav')
         self.dim_red_embeddings = np.load(dim_red_embeddings_file)
         self.umap_model = joblib.load(umap_model_file)
+        
+        #Download link for dim_red_embeddings
+        #!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1OieSf2kPtyTYuTiYJSTsNa7jpCsaQnum' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1OieSf2kPtyTYuTiYJSTsNa7jpCsaQnum" -O all-the-news-embeddings-title-umap.npy && rm -rf /tmp/cookies.txt
+        
+        #Download link for umap_model_file
+        #!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ibBI4BMS6zKfjPWarxdNKNvOuIt5Mj2D' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ibBI4BMS6zKfjPWarxdNKNvOuIt5Mj2D" -O umap-model.sav && rm -rf /tmp/cookies.txt
         
         print('Clustering model')
         #Load the Clustering model
@@ -117,6 +131,13 @@ class Prediction:
         f.close()
         self.labels = np.load(labels_file)
         
+        #Download link for kmeans_model
+        #!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qBL6k2w06IbXs1h_tNJb52QW6gkKhBGy' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qBL6k2w06IbXs1h_tNJb52QW6gkKhBGy" -O kmeans_model.p && rm -rf /tmp/cookies.txt
+        
+        #Download link for labels_file
+        #!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1bNF2rhe2rdG7zzPoay_NziXAjLtwLXhg' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1bNF2rhe2rdG7zzPoay_NziXAjLtwLXhg" -O umap-kmeans-labels.npy && rm -rf /tmp/cookies.txt
+        
+        
         print('Loading the topics file')
         #Load the topics file
         topics_file = os.path.join(BASE_PATH, 'umap-kmeans-topics.p')
@@ -124,13 +145,23 @@ class Prediction:
         self.topics = pickle.load(f)
         f.close()
         
+        #Download link for topics_file
+        #!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-1LXRgomfpqloQEvbDgfR_Hx7SAnOfWD' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-1LXRgomfpqloQEvbDgfR_Hx7SAnOfWD" -O umap-kmeans-topics.p && rm -rf /tmp/cookies.txt
+        
+        
+        
         print('Loading the topic labels file')
         #Load the topic labels file
         topic_labels_file = os.path.join(BASE_PATH, 'umap-kmeans-topic-labels.p')
         f = open(topic_labels_file, 'rb')
         self.topic_labels = pickle.load(f)
         f.close()
-
+        
+        
+        #Download link for topic_labels_file
+        #!wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-3p3vZYdY8iytnDn5gUaUVcabKfkrUhq' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-3p3vZYdY8iytnDn5gUaUVcabKfkrUhq" -O umap-kmeans-topic-labels.p && rm -rf /tmp/cookies.txt
+        
+        
         print('Initializing the feature extractor')
         self.feature_extractor = FeatureExtraction()
         
